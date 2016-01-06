1381d415fbe945276af7e059717fc743c337856f
Mingle Chang <mingle890805@gmail.com>
2016-01-06 20:25:02 +0800

    Initial Commit

---
+++ QRCode_OC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QRCode_OC
+//
+//  Created by 常峻玮 on 16/1/6.
+//  Copyright © 2016年 Mingle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ QRCode_OC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QRCode_OC
+//
+//  Created by 常峻玮 on 16/1/6.
+//  Copyright © 2016年 Mingle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
